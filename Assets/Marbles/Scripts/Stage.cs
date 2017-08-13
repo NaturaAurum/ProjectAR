5068aea2c9503175169d3e0b92864864ecb8c410
NaturaAurum <natur0321@gmail.com>
2017-08-14 00:15:59 +0900

    Can edit stage height in build

--- Assets/Marbles/Scripts/Stage.cs
+++ Assets/Marbles/Scripts/Stage.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.iOS;
+using UnityEngine.UI;
 
 public class Stage : MonoBehaviour
 {
+
+    public float HeightMax = 2.0f;
+    public Slider uiSlider = null;
+    private Vector3 firstPlanePos = Vector3.zero;
+
     /// <summary>
     /// Awake is called when the script instance is being loaded.
     /// </summary>
@@ -15,20 +21,26 @@ public class Stage : MonoBehaviour
 
     void ARAnchorAdded(ARPlaneAnchor anchor)
     {
-		var planePos = UnityARMatrixOps.GetPosition(anchor.transform);
-		var planeRot = UnityARMatrixOps.GetRotation(anchor.transform);
-		planePos.y += 0.15f;
-		transform.position = planePos;
-		transform.rotation = planeRot;
-		Debug.Log("Camera to first plane : " + (Camera.main.transform.position - planePos).magnitude);
-		UnityARSessionNativeInterface.ARAnchorAddedEvent -= ARAnchorAdded;	
+        var planePos = UnityARMatrixOps.GetPosition(anchor.transform);
+        var planeRot = UnityARMatrixOps.GetRotation(anchor.transform);
+        firstPlanePos = planePos;
+        planePos.y += 0.15f;
+        transform.position = planePos;
+        transform.rotation = planeRot;
+        Debug.Log("Camera to first plane : " + (Camera.main.transform.position - planePos).magnitude);
+        UnityARSessionNativeInterface.ARAnchorAddedEvent -= ARAnchorAdded;
     }
 
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
     /// </summary>
-    // void Update()
-    // {
-
-    // }
+    void Update()
+    {
+        if (uiSlider)
+        {
+            var stagePos = firstPlanePos;
+            stagePos.y += Mathf.Lerp(0, HeightMax, uiSlider.value);
+            transform.position = stagePos;
+        }
+    }
 }
