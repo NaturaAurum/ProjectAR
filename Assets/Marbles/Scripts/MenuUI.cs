c85d11c70ff1669385ef9d7fad0cd932d839141e
NaturaAurum <natur0321@gmail.com>
2017-08-16 01:52:54 +0900

    버튼 sender를 알기위해 커스텀으로 추가

--- Assets/Marbles/Scripts/MenuUI.cs
+++ Assets/Marbles/Scripts/MenuUI.cs
@@ -1,24 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using ProjectAR.Assets.Marbles.Scripts;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MenuUI : MonoBehaviour
 {
 
-    public Button singleButton;
-    public Button multiButton;
+    public CustomButton singleButton;
+    public CustomButton multiButton;
     public GameObject MultiOption;
-    private Button[] multiButtons;
+    private CustomButton[] multiButtons;
 
     private void Awake()
     {
-        
+        MultiOption.SetActive(false);
+        var optionCount = MultiOption.transform.childCount;
+        multiButtons = new CustomButton[optionCount];
+        for(int i = 0; i < optionCount; ++i){
+            multiButtons[i] = MultiOption.transform.GetChild(i).GetComponent<CustomButton>();
+            multiButtons[i].OnClick.AddListener(SelectMulti);
+        }
+
+        singleButton.OnClick.AddListener(DoSingle);
+        multiButton.OnClick.AddListener(DoMulti);
     }
 
     private void DoSingle()
     {
-
+        
     }
 
     private void DoMulti()
@@ -26,7 +36,7 @@ public class MenuUI : MonoBehaviour
 
     }
 
-    private void SelectMulti()
+    private void SelectMulti(GameObject sender)
     {
 
     }
