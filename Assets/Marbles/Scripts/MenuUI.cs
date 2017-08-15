c8ac7875c4e790e31fd73ad97b31cc1ce96b34be
DESKTOP-86NU4C3\taewoo.kim <natura0321@gmail.com>
2017-08-15 23:14:59 +0900

    add screen change effect, and update text mesh pro

---
+++ Assets/Marbles/Scripts/MenuUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuUI : MonoBehaviour
+{
+
+    public Button singleButton;
+    public Button multiButton;
+    public GameObject MultiOption;
+    private Button[] multiButtons;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void DoSingle()
+    {
+
+    }
+
+    private void DoMulti()
+    {
+
+    }
+
+    private void SelectMulti()
+    {
+
+    }
+}
